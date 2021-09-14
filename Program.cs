e2fa49b990592e82dd3df3a4b4f674bf24ecbe2c
Vitória Cristiana <vitoriacristiana290@gmail.com>
2021-09-14 14:19:28 -0300

    Resolução Exercício

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GrausCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c, convf;
+            Console.Beep();
+            Console.Clear();
+            Console.Write("Digite o valor em ºC:");
+            c = double.Parse(Console.ReadLine());
+            convf = c * 1.8 + 3;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"/n{convf} ºF");
+            Console.ResetColor();
+
+        }
+    }
+}
